ad7c238795af10f35153f8f4f027ce81808dbf13
SamudraW <152139583+SamudraW@users.noreply.github.com>
2026-02-24 23:05:23 +0530

    chnages done for api alls

--- Services/ai_prediction_service.js
+++ Services/ai_prediction_service.js
@@ -4,7 +4,7 @@ class AiPredictionService {
 
   async recommend(data) {
     const response = await axios.post(
-      "http://10.35.236.205:5001/api/ai/recommend",
+      "http://10.35.236.24:5001/api/ai/recommend",
       data
     );
     return response.data;
@@ -12,7 +12,7 @@ class AiPredictionService {
 
   async predict(data) {
     const response = await axios.post(
-      "http://10.35.236.205:5001/api/ai/pregnancy",
+      "http://10.35.236.24:5001/api/ai/pregnancy",
       data
     );
     return response.data;
