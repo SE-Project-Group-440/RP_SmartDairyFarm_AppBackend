6b8319a462dcbca436dbf60f32f0f5db0a8fe73c
Anjana Gunarathna <152806674+AnjanaGunarathna@users.noreply.github.com>
2026-04-12 17:42:07 +0530

    update

--- Services/HeatStressService.js
+++ Services/HeatStressService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getHeatPrediction = async (collarId) => {
   try {
     const res = await axios.get(
-      `${process.env.FASTAPI_BACKEND}/api/predict/${collarId}`,
+      `${process.env.ML_API_BASE}/api/predict/${collarId}`,
       { timeout: 5000 }
     );
     return res.data;
